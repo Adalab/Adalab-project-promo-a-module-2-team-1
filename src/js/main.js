ca014aa6152a7569cc24c6bf85aad5351a06371f
laumonmay <laumonmay@hotmail.com>
2023-11-22 23:23:15 +0100

    Share section.html/sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const downElement=document.querySelector('.js_down');
+const formElement=document.querySelector('.js_form');
+const pantoneElement=document.querySelector('.js_pantone');
+
+
+
+downElement.addEventListener('click', (event) => {
+
+});
