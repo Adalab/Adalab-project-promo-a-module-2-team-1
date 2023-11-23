efea00ea8b57cb6aec457b86713130e7298b9915
laumonmay <laumonmay@hotmail.com>
2023-11-23 14:26:50 +0100

    Desing. HTML y SASS

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-'use strict';
+/*'use strict';
 
 console.log('>> Ready :)');
 
+
+const headerElement=document.querySelector('.js_header');
 const downElement=document.querySelector('.js_down');
-const formElement=document.querySelector('.js_form');
 const pantoneElement=document.querySelector('.js_pantone');
 
-
+const oneElement=document.querySelector('.js_1');
+const twoElement=document.querySelector('.js_2');
+const threeElement=document.querySelector('.js_3');
 
 downElement.addEventListener('click', (event) => {
+    downElement.classList.toggle('rotate')
+});*/
 
-});
+
+
+
+
+
