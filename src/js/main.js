830f7d5200246c1188c4d8bf2ff541285d255f37
laumonmay <laumonmay@hotmail.com>
2023-11-24 13:42:54 +0100

    color/share. JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,33 @@
-/*'use strict';
+'use strict';
 
 console.log('>> Ready :)');
 
 
-const headerElement=document.querySelector('.js_header');
-const downElement=document.querySelector('.js_down');
-const pantoneElement=document.querySelector('.js_pantone');
+const ddownElement = document.querySelector('.js_ddown');
+const pantoneElement = document.querySelector('.js_pantone');
 
-const oneElement=document.querySelector('.js_1');
-const twoElement=document.querySelector('.js_2');
-const threeElement=document.querySelector('.js_3');
+const sdownElement = document.querySelector('.js_sdown');
+const cardElement = document.querySelector('.js_card');
 
-downElement.addEventListener('click', (event) => {
-    downElement.classList.toggle('rotate')
-});*/
+
+ddownElement.addEventListener('click', (event) => {
+    ddownElement.classList.toggle('desing__rotate')
+});
+ddownElement.addEventListener('click', (event) => {
+    pantoneElement.classList.toggle('desing__hidden')
+});
+
+
+sdownElement.addEventListener('click', (event) => {
+    sdownElement.classList.toggle('share__rotate')
+});
+
+
+sdownElement.addEventListener('click', (event) => {
+    cardElement.classList.toggle('share__hidden')
+});
+
+
 
 
 
