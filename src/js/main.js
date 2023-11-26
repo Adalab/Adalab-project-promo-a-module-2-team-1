c208d1b134313e5a677ca08afb826604518f5a8c
Marta <marta.af.lab@gmail.com>
2023-11-26 19:06:51 +0100

    stuffed

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,9 @@ const pantoneElement = document.querySelector('.js_pantone');
 const sdownElement = document.querySelector('.js_sdown');
 const cardElement = document.querySelector('.js_card');
 
+const stuffed = document.querySelector('.js_cont-stuffed');
+const icionstuffed = document.querySelector('.js_down-stuffed');
+
 
 ddownElement.addEventListener('click', (event) => {
     ddownElement.classList.toggle('desing__rotate')
@@ -27,8 +30,13 @@ sdownElement.addEventListener('click', (event) => {
     cardElement.classList.toggle('share__hidden')
 });
 
+icionstuffed.addEventListener('click', (event) => {
+   stuffed.classList.toogle('stuffed-hidden')
+});
 
-
+icionstuffed.addEventListener('click', (event) => {
+    stuffed.classList.toggle('rotate-stuffed')
+});
 
 
 
